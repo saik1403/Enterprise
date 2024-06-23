aa2cde3cf93b3e0ce0aa94e9d758a627b53bca5b
saik1403 <saikirankolluru32@gmail.com>
2024-06-23 15:58:53 +0530

    Task: add a model and makemigrations and migrate the migration file

--- temp/models.py
+++ temp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class temp(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
