b3e471ec2dc847d6f29c64f2fb61de0faa490e6a
saik1403 <saikirankolluru32@gmail.com>
2024-07-13 22:50:29 +0530

    Exercise 2 Completed

--- exercise_1/models.py
+++ exercise_1/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 import uuid
 
+class User(models.Model):
+    Email: str = models.CharField(max_length=50)
+
+    @property
+    def username(self):
+        return self.Email
+
+    @username.setter
+    def set_username(self, username):
+        self.Email = username
+
+    def __str__(self) -> str:
+        return self.Email
+
 class Country(models.Model):
     id: uuid.UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name: str = models.CharField(max_length=100)
     country_code: str = models.CharField(max_length=3)
     curr_symbol: str = models.CharField(max_length=3)
     phone_code: str = models.CharField(max_length=5)
+    my_user: User = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
